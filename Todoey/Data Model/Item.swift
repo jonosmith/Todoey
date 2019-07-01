1e11166e33f92b66329dfe2cd5bdf5ddd99b63d5
Jonathan Smith <jonathansmith@hotmail.com.au>
2019-07-01 19:53:24 +1000

    Convert to MVC pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Jonathan Smith on 30/6/19.
+//  Copyright © 2019 Jonathan Smith. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
